189a62064ba3a058467d76ab042d5a8c3bc00747
Davino Rizqy Dayan <vino.raisya@gmail.com>
2024-11-09 10:49:46 +0700

    added view product, view blog, view product category, search result for product (layout only)

--- app/src/main/java/com/example/tokoponik/HomeFragment.kt
+++ app/src/main/java/com/example/tokoponik/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.tokoponik
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,14 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    //variabel button
+    private lateinit var imgbtnCart: ImageButton
+    private lateinit var tv_see_all_product: TextView
+    private lateinit var tv_see_all_blog: TextView
+    private lateinit var imgbtnVegetable: ImageButton
+    private lateinit var imgbtnSeed: ImageButton
+    private lateinit var imgbtnTools: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,6 +46,47 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    // function intent
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Contoh penggunaan tombol untuk membuka Activity
+        imgbtnCart = view.findViewById(R.id.imgbtn_to_cart)
+        imgbtnCart.setOnClickListener {
+            val intent = Intent(activity, Cart::class.java)
+            startActivity(intent)
+        }
+
+        tv_see_all_product = view.findViewById(R.id.tv_see_all_product)
+        tv_see_all_product.setOnClickListener {
+            val intent = Intent(activity, ViewProduct::class.java)
+            startActivity(intent)
+        }
+
+        tv_see_all_blog = view.findViewById(R.id.tv_see_all_blog)
+        tv_see_all_blog.setOnClickListener {
+            val intent = Intent(activity, ViewBlog::class.java)
+            startActivity(intent)
+        }
+
+        imgbtnVegetable = view.findViewById(R.id.imgbtn_vegetable_category)
+        imgbtnVegetable.setOnClickListener {
+            val intent = Intent(activity, ViewProductCategory::class.java)
+            startActivity(intent)
+        }
+
+        imgbtnSeed = view.findViewById(R.id.imgbtn_seed_category)
+        imgbtnSeed.setOnClickListener {
+            val intent = Intent(activity, ViewProductCategory::class.java)
+            startActivity(intent)
+        }
+
+        imgbtnTools = view.findViewById(R.id.imgbtn_tools_category)
+        imgbtnTools.setOnClickListener {
+            val intent = Intent(activity, ViewProductCategory::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/example/tokoponik/ViewProductCategory.kt
@@ -0,0 +1,38 @@
+package com.example.tokoponik
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewProductCategory : AppCompatActivity() {
+
+    private lateinit var imgbtn_home: ImageButton
+    private lateinit var imgbtn_cart: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_product_category)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imgbtn_home = findViewById(R.id.imgbtn_to_home)
+        imgbtn_home.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        imgbtn_cart = findViewById(R.id.imgbtn_to_cart)
+        imgbtn_cart.setOnClickListener {
+            val intent = Intent(this, Cart::class.java)
+            startActivity(intent)
+        }
+    }
+}
