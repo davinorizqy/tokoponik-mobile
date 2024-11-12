05053fccd824b75f213102ae9840b25929696ce9
foxvorism <imjohani2005@gmail.com>
2024-11-12 11:29:08 +0700

    feat: Implement okhttp to set token in header

--- app/src/main/java/com/example/tokoponik/restapi/ApiClient.kt
+++ app/src/main/java/com/example/tokoponik/restapi/ApiClient.kt
@@ -1,6 +1,10 @@
 package com.example.tokoponik.restapi
 
+import com.example.tokoponik.helper.SessionManager
 import com.example.tokoponik.restapi.services.AddressService
+import com.example.tokoponik.restapi.services.AuthService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,6 +16,33 @@ object ApiClient {
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    fun getOkHttpClient(session: SessionManager): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging) // Logging for debugging
+            .addInterceptor { chain ->
+                val requestBuilder = chain.request().newBuilder()
+                val token = session.getAuthToken()
+
+                // Add Authorization header if token exists
+                if (token != null) {
+                    requestBuilder.addHeader("Authorization", "Bearer $token")
+                }
+
+                // Add Accept header
+                requestBuilder.addHeader("Accept", "application/json")
+
+                chain.proceed(requestBuilder.build())
+            }
+            .build()
+    }
+
+    val authService: AuthService by lazy {
+        retrofit.create(AuthService::class.java)
     }
 
     val addressService: AddressService by lazy {
