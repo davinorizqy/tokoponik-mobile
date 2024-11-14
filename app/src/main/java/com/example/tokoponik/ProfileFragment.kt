73e96acbda7a0ece22b862543715a67512cfa409
foxvorism <imjohani2005@gmail.com>
2024-11-14 12:16:22 +0700

    feat: Call get product function

--- app/src/main/java/com/example/tokoponik/ProfileFragment.kt
+++ app/src/main/java/com/example/tokoponik/ProfileFragment.kt
@@ -2,12 +2,24 @@ package com.example.tokoponik
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.tokoponik.helper.SessionManager
+import com.example.tokoponik.restapi.ApiClient
+import com.example.tokoponik.restapi.models.user.logoutResponse
+import com.example.tokoponik.restapi.services.AuthService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,8 +42,14 @@ class ProfileFragment : Fragment() {
     private lateinit var imgbtnLogout: ImageButton
     private lateinit var btnToAddress: Button
 
+    private lateinit var session: SessionManager
+    private lateinit var call: Call<logoutResponse>
+    private lateinit var authService: AuthService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        session = SessionManager(requireContext())
+        authService = ApiClient.getAuthService(session)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -55,6 +73,7 @@ class ProfileFragment : Fragment() {
         btnToEditProfile = view.findViewById(R.id.btn_to_editprofile)
         imgbtnLogout = view.findViewById(R.id.imgbtn_logout)
         btnToAddress = view.findViewById(R.id.btn_to_address)
+
         btnToEditAccount.setOnClickListener {
             val intent = Intent(activity, EditAccount::class.java)
             startActivity(intent)
@@ -64,13 +83,56 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
         imgbtnLogout.setOnClickListener {
-            val intent = Intent(activity, Login::class.java)
-            startActivity(intent)
+            AlertDialog.Builder(requireContext())
+                .setTitle("Konfirmasi Logout")
+                .setMessage("Kamu yakin ingin logout dari akun ini?")
+                .setPositiveButton("Delete") { dialog, _ ->
+                    logout()
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
         }
         btnToAddress.setOnClickListener {
             val intent = Intent(activity, ViewAddress::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun logout() {
+        call = authService.logout()
+
+        call.enqueue(object : Callback<logoutResponse> {
+            override fun onResponse(
+                call: Call<logoutResponse>,
+                response: Response<logoutResponse>
+            ) {
+                if (response.isSuccessful) {
+                    // Clear session data
+                    session.clearAuthToken()
+
+                    // Inform user and redirect to login
+                    Toast.makeText(requireContext(), "Logged out successfully", Toast.LENGTH_SHORT).show()
+
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        val intent = Intent(activity, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    }, 1000)
+                } else {
+                    Toast.makeText(requireContext(), "Logout failed", Toast.LENGTH_SHORT).show()
+                    Log.d("Not Success", response.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<logoutResponse>, t: Throwable) {
+                Toast.makeText(requireContext(), t.localizedMessage, Toast.LENGTH_SHORT).show()
+                Log.d("Error onFailure", t.localizedMessage)
+            }
+
+        })
     }
 
     companion object {
--- app/src/main/java/com/example/tokoponik/ViewProduct.kt
+++ app/src/main/java/com/example/tokoponik/ViewProduct.kt
@@ -2,21 +2,46 @@ package com.example.tokoponik
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tokoponik.helper.SessionManager
+import com.example.tokoponik.restapi.ApiClient
+import com.example.tokoponik.restapi.adapter.ProductAdapter
+import com.example.tokoponik.restapi.models.product.Product
+import com.example.tokoponik.restapi.models.product.getResponse
+import com.example.tokoponik.restapi.services.ProductService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewProduct : AppCompatActivity() {
 
     private lateinit var imgbtn_home: ImageButton
     private lateinit var imgbtn_cart: ImageButton
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var callGet: Call<getResponse>
+    private lateinit var sessionManager: SessionManager
+    private lateinit var productService: ProductService
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_product)
+
+        recyclerView = findViewById(R.id.productRecyclerView)
+        productAdapter = ProductAdapter { product: Product -> productOnClick(product) }
+        recyclerView.adapter = productAdapter
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -34,5 +59,37 @@ class ViewProduct : AppCompatActivity() {
             val intent = Intent(this, Cart::class.java)
             startActivity(intent)
         }
+
+        sessionManager = SessionManager(this)
+        productService = ApiClient.getProductService(sessionManager)
+
+        getProducts()
+    }
+
+    private fun productOnClick(product: Product) {
+        Toast.makeText(applicationContext, product.name, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getProducts () {
+        callGet = productService.getAllProduct()
+        callGet.enqueue(object : Callback<getResponse> {
+            override fun onResponse(
+                call: Call<getResponse>,
+                response: Response<getResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("Data Address", response.body()?.data.toString())
+                    productAdapter.submitList(response.body()?.data)
+                    productAdapter.notifyDataSetChanged()
+                } else {
+                    Log.d("Not Success", response.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<getResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, t.localizedMessage, Toast.LENGTH_SHORT).show()
+                Log.d("Error onFailure", t.localizedMessage)
+            }
+        })
     }
 }
