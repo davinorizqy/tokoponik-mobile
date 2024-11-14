e7ab0f3cba0ffae7cb77c76cdf7f513c69e2d3d2
foxvorism <imjohani2005@gmail.com>
2024-11-14 12:19:52 +0700

    feat: Create check session function

--- app/src/main/java/com/example/tokoponik/MainActivity.kt
+++ app/src/main/java/com/example/tokoponik/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tokoponik.helper.SessionManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var session: SessionManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,10 +24,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        session = SessionManager(this)
+        val token = session.getAuthToken()
+        Log.d("Token", token?: "Token is NULL")
+
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+            if (token.isNullOrEmpty()) {
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
             finish()
-        }, 3000)
+        }, 1000)
     }
 }
