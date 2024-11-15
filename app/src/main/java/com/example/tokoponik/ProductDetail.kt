1529d4f2db44c3d77dbd948177c784f6977c0808
foxvorism <imjohani2005@gmail.com>
2024-11-15 15:21:48 +0700

    feat: Create get product detail function

--- app/src/main/java/com/example/tokoponik/ProductDetail.kt
+++ app/src/main/java/com/example/tokoponik/ProductDetail.kt
@@ -2,18 +2,31 @@ package com.example.tokoponik
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tokoponik.helper.SessionManager
+import com.example.tokoponik.restapi.ApiClient
+import com.example.tokoponik.restapi.models.product.productResponse
+import com.example.tokoponik.restapi.services.ProductService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductDetail : AppCompatActivity() {
 
     private lateinit var imgbtnBackToProduct: ImageButton
     private lateinit var imgbtnToCart: ImageButton
     private lateinit var btnToReview: Button
+
+    private lateinit var callGet: Call<productResponse>
+    private lateinit var sessionManager: SessionManager
+    private lateinit var productService: ProductService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +55,38 @@ class ProductDetail : AppCompatActivity() {
             val intent = Intent(this, ViewReview::class.java)
             startActivity(intent)
         }
+
+        val productId = intent.getIntExtra("product_id", -1)
+
+        sessionManager = SessionManager(this)
+        productService = ApiClient.getProductService(sessionManager)
+
+        if (productId != -1) {
+            Log.d("Product Id", productId.toString())
+            getProductDetail(productId)
+        } else {
+            Log.e("ProductDetail", "Invalid Product ID")
+        }
+    }
+
+    private fun getProductDetail(productId: Int) {
+        callGet = productService.getProductById(productId)
+        callGet.enqueue(object : Callback<productResponse> {
+            override fun onResponse(
+                call: Call<productResponse>,
+                response: Response<productResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("Data Product", response.body()?.data.toString())
+                } else {
+                    Log.d("Not Success", response.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<productResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, t.localizedMessage, Toast.LENGTH_SHORT).show()
+                Log.d("Error onFailure", t.localizedMessage)
+            }
+        })
     }
 }
