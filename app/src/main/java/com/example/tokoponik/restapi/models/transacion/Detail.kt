acaebf2800930101ed3b50ccd0a627370a1dcec3
foxvorism <imjohani2005@gmail.com>
2024-11-22 21:17:51 +0700

    feat: Create detail product model

---
+++ app/src/main/java/com/example/tokoponik/restapi/models/transacion/Detail.kt
@@ -0,0 +1,14 @@
+package com.example.tokoponik.restapi.models.transacion
+
+import android.os.Parcelable
+import com.example.tokoponik.restapi.models.product.Product
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Detail(
+    val id: Int,
+    val transaction_id: Int,
+    val product_id: Int,
+    val qty: Int,
+    val product: Product,
+) : Parcelable
